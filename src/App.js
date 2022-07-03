51010219d96fb26ea25772777bc5e5d5e9c71a71
Md. Masud Rana <mr.hasib45@gmail.com>
2022-07-03 18:07:23 +0600

    email password authentication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import initializeAuthentication from "./Firebase/firebase.initialize";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  sendEmailVerification,
+} from "firebase/auth";
+
+initializeAuthentication();
 
 function App() {
+  const auth = getAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSingIn, setIsSingIn] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleChangePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleIsLoggedIn = (e) => {
+    setIsSingIn(e.target.checked);
+  };
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    // if (password.length < 8) {
+    //   setError("Password should be at least 8 ");
+    //   return;
+    // }
+    if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+      setError("password must contain 2 upper case");
+      return;
+    }
+
+    if (isSingIn) {
+      handleSingIn();
+      setError("");
+    } else {
+      handleSignUp();
+      setError("");
+    }
+    console.log(email, password);
+  };
+
+  const handleSingIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const handleSignUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        verifyEmail();
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then((result) => {});
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-5">
+      <div>
+        <form onSubmit={handleRegistration}>
+          <h3 className="text-primary">
+            {isSingIn ? "Sign In" : "Registration"}
+          </h3>
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              Email address
+            </label>
+            <input
+              type="email"
+              onBlur={handleChangeEmail}
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              required
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              onBlur={handleChangePassword}
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              required
+            />
+          </div>
+          <div className="mb-3 form-check">
+            <input
+              onChange={handleIsLoggedIn}
+              type="checkbox"
+              className="form-check-input"
+              id="exampleCheck1"
+            />
+            <label className="form-check-label" htmlFor="exampleCheck1">
+              Already LoggedIn
+            </label>
+          </div>
+          <div className="text-danger">
+            <p>{error}</p>
+          </div>
+          <button type="submit" className="btn btn-primary">
+            {isSingIn ? "Login" : "Register"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDsR1PTvAH9ONFYoZ1ag7dVhYTdhnI6ZK0",
+  authDomain: "react-firebase-auth-8fc23.firebaseapp.com",
+  projectId: "react-firebase-auth-8fc23",
+  storageBucket: "react-firebase-auth-8fc23.appspot.com",
+  messagingSenderId: "863877112927",
+  appId: "1:863877112927:web:e26d8d7b75113ac664a7ea"
+};
+
+export default firebaseConfig;
